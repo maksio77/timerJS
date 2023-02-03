a36e28d6f48a56af15e25bbdb9a4f4f2ba1c4cb2
Максим Павлів <maksio@iMac-Maksim.local>
2023-02-04 00:10:02 +0200

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-function timer(id, deadline) {
-      function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          seconds = Math.floor((t / 1000) % 60),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    
-        return {
-          total: t,
-          days: days,
-          hours: hours,
-          minutes: minutes,
-          seconds: seconds,
-        };
+function slider() {
+  let slideIndex = 1;
+  const slides = document.querySelectorAll('.offer__slide'),
+      prev = document.querySelector('.offer__slider-prev'),
+      next = document.querySelector('.offer__slider-next'),
+      total = document.querySelector('#total'),
+      current = document.querySelector('#current');
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+  } else {
+      total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+      if (n > slides.length) {
+          slideIndex = 1;
       }
-    
-      function getZero(num) {
-        if (num >= 0 && num < 10) {
-          return "0" + num;
-        } else {
-          return num;
-        }
+      if (n < 1) {
+          slideIndex = slides.length;
       }
-    
-      function setClock(selector, endtime) {
-        const timer = document.querySelector(selector),
-          days = timer.querySelector("#days"),
-          hours = timer.querySelector("#hours"),
-          minutes = timer.querySelector("#minutes"),
-          seconds = timer.querySelector("#seconds"),
-          timeInterval = setInterval(updateClock, 1000);
-    
-        updateClock();
-    
-        function updateClock() {
-          const t = getTimeRemaining(endtime);
-    
-          days.innerHTML = getZero(t.days);
-          hours.innerHTML = getZero(t.hours);
-          minutes.innerHTML = getZero(t.minutes);
-          seconds.innerHTML = getZero(t.seconds);
-    
-          if (t.total <= 0) {
-            clearInterval(timeInterval);
-          }
-        }
+
+      slides.forEach((item) => item.style.display = 'none');
+
+      slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+      
+      if (slides.length < 10) {
+          current.textContent =  `0${slideIndex}`;
+      } else {
+          current.textContent =  slideIndex;
       }
-    
-      setClock(id, deadline);
-    }
-    
-    export default timer;
-    
+  }
+
+  function plusSlides (n) {
+      showSlides(slideIndex += n);
+  }
+
+  prev.addEventListener('click', function(){
+      plusSlides(-1);
+  });
+
+  next.addEventListener('click', function(){
+      plusSlides(1);
+  });
+}
+
+export default slider;
